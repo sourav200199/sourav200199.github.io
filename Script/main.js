791b0d04ac73b7c579d7bc982b2e8be003b6707e
Sourav Chakraborty <sourav.992001@gmail.com>
2023-03-09 12:07:07 +0530

    Updated

--- Script/main.js
+++ Script/main.js
@@ -1,62 +1,73 @@
-$(document).ready(function() 
-        {
-            $(".cross").hide();
-            $(".hamburger").click(function() {
-                $(".list-items").toggleClass("active");
-            });
+$(document).ready(function () {
+  $(".cross").hide();
+    $(".hamburger").click(function () {
+        $(".list-items").toggleClass("active");
+    });
+    $(".items").click(function () {
+        $(".list-items").removeClass("active");
+    });
 
-            $(window).scroll(function() {
-                if ($(this).scrollTop() > 100) {
-                    $(".menu").addClass("sticky");
-                    $("#surname").addClass("sticky");
-                } else {
-                    $(".menu").removeClass("sticky");
-                    $("#surname").removeClass("sticky");
-                }
-            });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".menu").addClass("sticky");
+      $("#surname").addClass("sticky");
+    } else {
+      $(".menu").removeClass("sticky");
+      $("#surname").removeClass("sticky");
+    }
+  });
+});
 
-        });
+$(document).on("scroll", function () {
+  var pixels = $(document).scrollTop();
+  var pageHeight = $(document).height() - $(window).height();
+  var progress = (100 * pixels) / pageHeight;
 
-        $(document).on("scroll", function(){
-            var pixels = $(document).scrollTop();
-            var pageHeight = $(document).height() - $(window).height();
-            var progress = 100 * pixels / pageHeight;
-  
-            $("#progress").css("width", progress + "%");
-        })
+  $("#progress").css("width", progress + "%");
+});
 
-        $(document).on({
-            mouseenter: function () {
-                $("#text1").css("display", "block");
-                $("#label1").css("display", "none");
-            },
+$(document).on(
+  {
+    mouseenter: function () {
+      $("#text1").css("display", "block");
+      $("#label1").css("display", "none");
+    },
 
-            mouseleave: function () {
-                $("#text1").css("display", "none");
-                $("#label1").css("display", "block");
-            }
-        }, '#card1');
+    mouseleave: function () {
+      $("#text1").css("display", "none");
+      $("#label1").css("display", "block");
+    },
+  },
+  "#card1"
+);
 
-        $(document).on({
-            mouseenter: function () {
-                $("#text2").css("display", "block");
-                $("#label2").css("display", "none");
-            },
+$(document).on(
+  {
+    mouseenter: function () {
+      $("#text2").css("display", "block");
+      $("#label2").css("display", "none");
+    },
 
-            mouseleave: function () {
-                $("#text2").css("display", "none");
-                $("#label2").css("display", "block");
-            }
-        }, '#card2');
+    mouseleave: function () {
+      $("#text2").css("display", "none");
+      $("#label2").css("display", "block");
+    },
+  },
+  "#card2"
+);
 
-        $(document).on({
-            mouseenter: function () {
-                $("#text3").css("display", "block");
-                $("#label3").css("display", "none");
-            },
+$(document).on(
+  {
+    mouseenter: function () {
+      $("#text3").css("display", "block");
+      $("#label3").css("display", "none");
+    },
 
-            mouseleave: function () {
-                $("#text3").css("display", "none");
-                $("#label3").css("display", "block");
-            }
-        }, '#card3');
+    mouseleave: function () {
+      $("#text3").css("display", "none");
+      $("#label3").css("display", "block");
+    },
+  },
+  "#card3"
+);
+
